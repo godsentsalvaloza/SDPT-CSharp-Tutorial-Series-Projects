ff04595ef0ecddb36a96163088665a63fa8ac718
Godsent <85384933+godsentsalvaloza@users.noreply.github.com>
2025-01-26 21:03:11 +0800

    Uploaded until tutorial 3 - User Input

---
+++ Tutorial #2 Variables - DataTypes/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sentence Builder Challenge
+            string name = "Sent";
+            int age = 18;
+            decimal GPA = 2.25m;
+            char sex = 'M';
+
+            // Using concatenation
+            //Console.WriteLine("Hi, my name is " + name);
+            //Console.WriteLine("I am " + age + " years old");
+            //Console.WriteLine("My GPA is " + GPA);
+            //Console.WriteLine("My sex is " + sex);
+
+            // Using interpolation
+            Console.WriteLine($"Hi, my name is {name}");
+            Console.WriteLine($"I am {age} years old");
+            Console.WriteLine($"My GPA is {GPA}");
+            Console.WriteLine($"My sex is {sex}");
+
+
+            // Variables and Datatypes
+            //string name = "sent";
+            //int age = 18;
+            //char sex = 'M';
+            //bool isTall = true;
+            //float aveGrade = 92.5f;
+            //double grade = 90.33;
+
+
+            //Assigning variables
+
+            //string name;
+            //name = "sent";
+            //int age = 18;
+
+            ////Console.Write("My name is ");
+            ////Console.WriteLine(name);
+
+            //Console.Write("My age is ");
+            //Console.WriteLine(age);
+
+            //age = 20;
+
+            //Console.Write("My age is ");
+            //Console.WriteLine(age);
+
+            //// Concatenation
+            //Console.WriteLine("Hello " + "World");
+
+            //string name = "Sent";
+            //Console.WriteLine("My name is " + name);
+        }
+    }
+}
---
+++ Tutorial #3 User Input/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sentence Builder Challenge
+            string name, location, school;
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter your location: ");
+            location = Console.ReadLine();
+
+            Console.Write("Enter your school: ");
+            school = Console.ReadLine();
+
+            Console.WriteLine($"Hi! My name is {name}");
+            Console.WriteLine($"I am from {location}");
+            Console.WriteLine($"I study in {school}");
+
+
+
+            // ReadLine
+            //string name;
+            //Console.Write("Enter your name: ");
+            //name = Console.ReadLine();
+            //Console.Write($"Hello, {name}");
+
+            // Read
+            //char letter;
+            //Console.Write("Enter letter: ");
+            //letter = (char) Console.Read();
+
+            //ReadKey
+            //char letter;
+            //Console.Write("Enter letter: ");
+            //letter = Console.ReadKey().KeyChar;
+
+        }
+    }
+}
