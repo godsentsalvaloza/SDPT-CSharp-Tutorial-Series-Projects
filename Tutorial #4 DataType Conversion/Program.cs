44c0e44f58228e2dad5e6c22e63982399678b181
Godsent <85384933+godsentsalvaloza@users.noreply.github.com>
2025-01-30 21:22:28 +0800

    Uploaded until tutorial 5 - arithmetic operations

---
+++ Tutorial #4 DataType Conversion/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Datatype conversion
+            // implicit casting - smaller value to larger value
+
+            int myInt = 3;
+            double myFloat = myInt;
+
+            char letter = 'A';
+            int ascii = letter;
+
+            //Console.WriteLine(myInt);
+            //Console.WriteLine(myFloat);
+
+            //Console.WriteLine(letter);
+            //Console.WriteLine(ascii);
+
+            // explicit casting - larger value to smaller value
+
+            int code = 66;
+            char newCode = (char) code;
+
+            double myDouble = 32.32121212;
+            int newInteger = (int) myDouble;
+
+            // Conversion Method
+
+            //int num1, num2;
+
+            //Console.Write("Enter first number: ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter second number: ");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"The sum of two numbers is {num1 + num2}");
+
+            int num1, num2;
+
+            Console.Write("Enter first number: ");
+            string input = Console.ReadLine();
+
+            Console.Write("Enter second number: ");
+            string input2 = Console.ReadLine();
+
+            bool test1 = int.TryParse(input, out num1);
+            bool test2 = int.TryParse(input2 , out num2);
+
+            if (test1 && test2)
+            {
+                Console.WriteLine($"The sum of two numbers is {num1 + num2}");
+            } else
+            {
+                Console.WriteLine("Error");
+            }
+
+        }
+    }
+}
---
+++ Tutorial #5 Arithmetic Operators/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Average Calculator
+            string english, math, science, computer;
+            double val1 = 0, val2 = 0, val3 = 0, val4 = 0, result = 0;
+
+            Console.Write("Enter English Grade: ");
+            english = Console.ReadLine();
+
+            Console.Write("Enter Math Grade: ");
+            math = Console.ReadLine();
+
+            Console.Write("Enter Science Grade: ");
+            science = Console.ReadLine();
+
+            Console.Write("Enter Computer Grade: ");
+            computer = Console.ReadLine();
+
+            bool check = double.TryParse(english, out val1) && double.TryParse(math, out val2) && double.TryParse(science, out val3) && double.TryParse(computer, out val4);
+
+            if(check)
+            {
+                Console.WriteLine($"English   : {val1}");
+                Console.WriteLine($"Math      : {val2}");
+                Console.WriteLine($"Science   : {val3}");
+                Console.WriteLine($"Computer  : {val4}");
+                Console.WriteLine($"Average   : {(val1 + val2 + val3 + val4) / 4}");
+            } else
+            {
+                Console.WriteLine("Error!");
+            }
+
+            // Basic arithmetic operation
+
+            int num1 = 10, num2 = 5;
+
+            int sum = num1 + num2;
+            int sub = num1 - num2;
+            int prod = num1 * num2;
+            int quo = num1 / num2;
+            int mod = num1 % num2;
+
+            //Console.WriteLine($"Sum: {sum}");
+            //Console.WriteLine($"Difference: {sub}");
+            //Console.WriteLine($"Product: {prod}");
+            //Console.WriteLine($"Quotient: {quo}");
+            //Console.WriteLine($"Modulo: {mod}");
+
+            //increment / Decrement
+
+            //int x = 5;
+            //x++;
+            //x--;
+
+            // order of operation
+            //float val1 = 10;
+            //float val2 = 5;
+
+            //Console.WriteLine($"{(val1 + val2) / 2}");
+
+            // Assignment arithmetic operators
+            // val1 = val1 + val2
+            val1 += val2;
+
+        }
+    }
+}
