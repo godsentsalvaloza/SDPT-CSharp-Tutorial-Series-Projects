34f7340c513127a7c035c2fa8422b60918328dd6
Godsent <85384933+godsentsalvaloza@users.noreply.github.com>
2025-02-02 20:57:42 +0800

    Uploaded until tutorial 7 - Conditional Statement

---
+++ Tutorial #6 Arrays/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Paired Arrays By Index - Challenge
+            string[] email = {"creamy@mail.com", "sentry@mail.com", "David@mail.com", "Spike@mail.com", "Alex@mail.com"};
+            string[] username = {"Creamy", "Sentry", "David", "Spike", "Alex"};
+            string[] password = {"h3eh2d8", "!@^F&^tsdsds", "UHDWH8934y3", "!#*DHheygfe", "2HW&*EG78egd3"};
+            int index = 4   ;
+
+            Console.WriteLine("Account Information");
+            Console.WriteLine($"Email    : {email[index]}");
+            Console.WriteLine($"Username : {username[index]}");
+            Console.WriteLine($"Password : {password[index]}");
+
+
+
+
+            // Declear with values
+            string[] names = {"Sentry", "Creamy", "Saki"};
+            float[] grades = {92.5f, 99.99f, 91.32f};
+
+            // Declear without values
+            string[] nicknames = new string[3];
+            int[] age = new int[3];
+
+            // displaying array value
+            //Console.WriteLine(names[0]);
+
+            // assigning array value
+            age[0] = 10;
+
+            // using user input
+            //Console.Write("Enter Nickname: ");
+            //names[0] = Console.ReadLine();
+            //Console.WriteLine($"Hello, {names[0]}");
+
+            //float[] rating = new float[3];
+            //Console.Write("Enter Rating: ");
+            //rating[0] = Convert.ToSingle(Console.ReadLine());
+            //Console.WriteLine($"Rating: {rating[0]}");
+
+
+        }
+    }
+}
---
+++ Tutorial #7 Conditional Statements/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Average Grade Calculator - Challenge
+            string[] input = new string[4];
+            decimal[] grades = new decimal[4];
+            decimal average;
+
+            Console.WriteLine("Average Grade Calculator");
+            Console.Write("Enter grade in Filipino  : ");
+            input[0] = Console.ReadLine();
+            Console.Write("Enter grade in English   : ");
+            input[1] = Console.ReadLine();
+            Console.Write("Enter grade in Computer  : ");
+            input[2] = Console.ReadLine();
+            Console.Write("Enter grade in Math      : ");
+            input[3] = Console.ReadLine();
+
+            if (decimal.TryParse(input[0], out grades[0]) && decimal.TryParse(input[1], out grades[1]) && decimal.TryParse(input[2], out grades[2]) && decimal.TryParse(input[3], out grades[3])){
+                average = (grades[0] + grades[1] + grades[2] + grades[3]) / 4;
+
+                Console.WriteLine("");
+                Console.WriteLine($"Average Grade: {average}");
+
+                if(average > 100)
+                {
+                    Console.WriteLine("ERROR: Invalid Grade!");
+                }else if(average >= 98)
+                {
+                    Console.WriteLine("You passed with HIGHEST HONORS!");
+                } else if (average >= 95)
+                {
+                    Console.WriteLine("You passed with HIGH HONORS!");
+                } else if(average >= 90)
+                {
+                    Console.WriteLine("You passed with HONORS!");
+                } else if(average >= 75)
+                {
+                    Console.WriteLine("You passed!");
+                } else if (average >= 0)
+                {
+                    Console.WriteLine("You Failed...");
+                }else
+                {
+                    Console.WriteLine("ERROR: Invalid Grade!");
+                }
+            } else
+            {
+                Console.WriteLine("ERROR: Invalid Input");
+            }
+        }
+    }
+}
