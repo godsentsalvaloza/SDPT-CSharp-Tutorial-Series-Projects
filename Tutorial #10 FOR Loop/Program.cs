092f946b68c1141330fc131d42cb278709e5e47f
Godsent <85384933+godsentsalvaloza@users.noreply.github.com>
2025-02-04 17:52:33 +0800

    Updated until tutorial 11 - FOREACH Loop

---
+++ Tutorial #10 FOR Loop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userName = {"Sentry", "Creamy", "Spike" };
+            string[] password = {"sentry123", "Creamy123", "Spiky33" };
+            string nameInput, passwordInupt, result = "ERROR: Account not Found";
+
+            Console.WriteLine("Please Authenticate");
+            Console.Write("Username : ");
+            nameInput = Console.ReadLine();
+            Console.Write("Password : ");
+            passwordInupt = Console.ReadLine();
+
+            for (int x = 0; x < userName.Length; x++)
+            {
+                if (userName[x].Equals(nameInput) && password[x].Equals(passwordInupt))
+                {
+                    result = $"WELCOME: {userName[x]}";
+                    break;
+                }
+                else if (userName[x].Equals(nameInput) && !password[x].Equals(passwordInupt))
+                {
+                    result = $"ERROR: Wrong password for {userName[x]}";
+                    break;
+                }
+                else
+                {
+                    result = $"ERROR: Account not Found";
+                }
+            }
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ Tutorial #11 FOREACH Loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array of integers summation
+            int[] numbers = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            int result = 0;
+            foreach (int number in numbers)
+            {
+                result += number;
+            }
+            Console.WriteLine($"The summation of all values are: {result}");
+            
+        }
+    }
+}
