9aad1736427120eb2a43a92469935fc3163ed2b9
Godsent <85384933+godsentsalvaloza@users.noreply.github.com>
2025-02-05 20:37:26 +0800

    Uploaded until tutorial 12 - Methods Functions

---
+++ Tutorial #12 2D Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Multi-dimetional Array
+            string[,] accountInfo = { { "Sentry", "sent9x" }, { "Creamy", "Creamy33" }, {"Spike", "spiky" } };
+            int index = 1;
+            // Using for loop
+            //for (int i = 0; i < accountInfo.GetLength(0); i++)
+            //{
+            //    for(int j = 0; j < accountInfo.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine(accountInfo[i,j]);
+            //    }
+            //    Console.WriteLine();    
+            //}
+
+            // Using for-each
+            foreach (string account in accountInfo)
+            {
+                Console.WriteLine(account);
+                if(index % 2 == 0)
+                {
+                    Console.WriteLine();
+                }
+                index++;
+            }
+
+
+        }
+    }
+}
---
+++ Tutorial #13 Methods Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Challenge activity - Summation Challenge
+            int[] values = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            Console.WriteLine(summation(values));
+        }
+        static int summation(int[] input)
+        {
+            int result = 0;
+            foreach(int i in input)
+            {
+                result+=i;
+            }
+            return result;
+        }
+    }
+}
