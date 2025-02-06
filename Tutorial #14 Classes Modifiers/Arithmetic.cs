dd1ca8095d82ea7fc9f0c6c0a49b8488c0a3f8f6
Godsent <85384933+godsentsalvaloza@users.noreply.github.com>
2025-02-06 21:08:38 +0800

    Uploaded until tutorial 14 - Classes Modifiers

---
+++ Tutorial #14 Classes Modifiers/Arithmetic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Arithmetic
+    {
+        public int add(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+        public int subt(int val1, int val2)
+        {
+            return val1 - val2;
+        }
+        public int mult(int val1, int val2)
+        {
+            return val1 * val2;
+        }
+        public int div(int val1, int val2)
+        {
+            return val1 / val2;
+        }
+    }
+}
---
+++ Tutorial #14 Classes Modifiers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Arithmetic arithmetic = new Arithmetic();
+            Console.WriteLine(arithmetic.add(1,1));
+            Console.WriteLine(arithmetic.subt(10, 5));
+            Console.WriteLine(arithmetic.mult(10, 5));
+            Console.WriteLine(arithmetic.div(10, 5));
+        }
+    }
+}
